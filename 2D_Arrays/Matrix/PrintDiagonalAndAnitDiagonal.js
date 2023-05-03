18ad7cab350cddfe4fa8469d2f778829b06792fd
Lalith Aditya <lalithaditya@Laliths-MacBook-Pro.local>
2023-05-03 19:20:21 +0530

    Solved Diagonlas Problem

--- 2D_Arrays/Matrix/PrintDiagonalAndAnitDiagonal.js
+++ 2D_Arrays/Matrix/PrintDiagonalAndAnitDiagonal.js
@@ -1,1 +1,27 @@
 /*Print the Diagonals and Anti Diagonals Of a Matrix */
+let matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+// Printing Diagnols
+
+let i = 0;
+let N = matrix[0].length;
+while (i < N) {
+  console.log(matrix[i][i]);
+  i++;
+}
+
+console.log("--------------------------");
+// Printing Anti Diagona;
+i = 0;
+let j = matrix[0].length - 1;
+
+while (i < N && j >= 0) {
+  console.log(matrix[i][j]);
+  i++;
+  j--;
+}
